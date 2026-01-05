caefc7f151e34f2d72f253d169b70bf847b29291
Patricio Tamés <patriciotames@gmail.com>
2026-01-05 13:32:10 +0100

    feat: Initializing Groq LLM

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -2,3 +2,15 @@ import os
 from typing import List, Dict
 from crewai import Agent, Task, Crew, Process
 from langchain_groq import ChatGroq
+import logging
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Initiazile Groq LLM
+llm = ChatGroq(
+    model="groq/llama-3.3-70b-versatile",
+    temperature=0.3,
+    api_key=os.getenv("GROQ_API_KEY")
+)
